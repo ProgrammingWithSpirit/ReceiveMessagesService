04ec2a7b51047c2dd270dad936fce77a38151df5
ProgrammimgWithSpirit <blogtatsiana2019@gmail.com>
2020-05-12 21:07:02 -0400

    Initial check-in of module ReceiveMessagesService

---
+++ BookConsumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+using SendMessagesApi;
+
+namespace ReceiveMessagesService
+{
+    public class BookConsumer : IConsumer<Book>
+    {
+        public Task Consume(ConsumeContext<Book> context)
+        {
+            var value = $"Received: book {context.Message.Title} with {context.Message.PagesCount} pages written on {context.Message.Date}";
+            return Task.CompletedTask;
+        }
+    }
+}
